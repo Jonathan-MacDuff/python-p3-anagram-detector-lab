19f00548bbc475df80444a2d08ef3aa08025fb69
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-04-17 13:13:11 -0700

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self._word = word
+
+    def match(self, anagrams):
+        anagram_array = []
+        for word in anagrams:
+            if [letter for letter in sorted(self._word)] == [letter for letter in sorted(word)]:
+                anagram_array.append(word)
+        return anagram_array
